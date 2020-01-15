9a1104a759b182f3da77825f662ff32881989a78
Thor D. Noe <36196997+thornoe@users.noreply.github.com>
2020-01-15 16:57:32 +0100

    .

--- code/data_streams.py
+++ code/data_streams.py
@@ -3,47 +3,34 @@ import numpy as np
 import pandas as pd
 import os
 
-# Work pc
-# os.chdir('C:/Users/jwz766/Documents/GitHub/gnnp') # one level up
-# Home pc
-os.chdir('C:/Users/thorn/OneDrive/Dokumenter/GitHub/gnnp') # one level up
+# Work pc:
+os.chdir('C:/Users/jwz766/Documents/GitHub/gnnp') # one level up
+# Home pc:
+# os.chdir('C:/Users/thorn/OneDrive/Dokumenter/GitHub/gnnp') # one level up
 
 # Danish Water Fauna Index (DVFI)
 wf = pd.read_excel('streams/DFI_1970-2000.xlsx')
+wf = wf[wf['Indekstype'].str.contains('DVFI')]
 
-wf.drop(columns=['MiljøcenterNavn', 'RegionNavn'], leve=1, inplace=True)
+wf['År'] = wf['Dato'].astype(str).str.slice(0,4).astype(int)
+wf = wf[['KommuneNavn', 'Lokalitetsnavn', 'MC-stationsnr', 'Indekstype', 'Indeks', 'År']]
 wf.head(1)
 
-len(wf['MarinReferenceKode'].value_counts())
-len(wf['Farvandsområde'].value_counts())
-len(wf['Lokalitetsnavn'].value_counts())
-len(wf['ObservationsStedNavn'].value_counts())
-len(wf['ObservationsStedNr'].value_counts())
-len(wf['MC-stationsnr'].value_counts())
+år, antal, kommuner = [], [], []
+
+for y in range(1990,2001):
+    df = wf[wf['År']==y]
+    l = df['KommuneNavn'].value_counts().index.to_list()
+    l.sort()
+    år.append(y)
+    antal.append(len(l))
+    kommuner.append(', '.join([str(elem) for elem in l]))
 
 
+år
+antal
+kommuner
+pd.DataFrame({'År':år, 'Antal kommuner':antal, 'Kommuner':kommuner})
 
-eg['MarinReferenceKode'].value_counts().count()
-eg['Farvandsområde'].value_counts().count()
-eg['Lokalitetsnavn'].value_counts().count()
-eg['ObservationsStedNr'].value_counts().describe()
-eg['MC-stationsnr'].value_counts().count()
-eg['StartDato'].value_counts().count()
-eg['Kvalitet'].value_counts().count()
-eg['Kontrol'].value_counts()
-eg['Kvalitetsnote'].value_counts().count()
-eg['Navn'].value_counts().count()
-eg['Beskrivelse'].value_counts().count()
-eg['Dækningsgrad i %'].value_counts().count()
-
-
-
-eg[eg['Lokalitetsnavn']=='Hevring Bugt']['Farvandsområde'].value_counts()
-eg[eg['Lokalitetsnavn']=='Hevring Bugt']['ObservationsStedNr'].value_counts()
-eg[eg['Lokalitetsnavn']=='Hevring Bugt']['Farvandsområde'].value_counts()
-
-
-eg['Lokalitetsnavn'].value_counts().nlargest(40)
-eg['Lokalitetsnavn'].value_counts().nsmallest(41)
-
-eg['Beskrivelse'].value_counts()
+wf['Lokalitetsnavn'].value_counts().nlargest(50)
+wf['Lokalitetsnavn'].value_counts().nsmallest(50)
