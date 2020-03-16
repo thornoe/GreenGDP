45a543ffc8df36eb90a3ffc229bc738f94264841
Thor D. Noe <36196997+thornoe@users.noreply.github.com>
2020-03-16 11:57:09 +0100

    data on streams

---
+++ gis/script.py
@@ -0,0 +1,37 @@
+# Imports
+import pandas as pd
+import os
+
+os.chdir('C:/Users/jwz766/Documents/GitHub/gnnp') # one level up
+
+# Eelgrass
+eg = pd.read_excel('sea/Hav_Vegetation_Paravane træk_1998-2009.xlsx')
+
+eg.head(1)
+
+
+
+eg['MarinReferenceKode'].value_counts().count()
+eg['Farvandsområde'].value_counts().count()
+eg['Lokalitetsnavn'].value_counts().count()
+eg['ObservationsStedNr'].value_counts().describe()
+eg['MC-stationsnr'].value_counts().count()
+eg['StartDato'].value_counts().count()
+eg['Kvalitet'].value_counts().count()
+eg['Kontrol'].value_counts()
+eg['Kvalitetsnote'].value_counts().count()
+eg['Navn'].value_counts().count()
+eg['Beskrivelse'].value_counts().count()
+eg['Dækningsgrad i %'].value_counts().count()
+
+
+
+eg[eg['Lokalitetsnavn']=='Hevring Bugt']['Farvandsområde'].value_counts()
+eg[eg['Lokalitetsnavn']=='Hevring Bugt']['ObservationsStedNr'].value_counts()
+eg[eg['Lokalitetsnavn']=='Hevring Bugt']['Farvandsområde'].value_counts()
+
+
+eg['Lokalitetsnavn'].value_counts().nlargest(40)
+eg['Lokalitetsnavn'].value_counts().nsmallest(41)
+
+eg['Beskrivelse'].value_counts()
